586638d7ff91fa541de297cb56ff501160cf9e47
Rehan FuduGo <rehanfudugo@gmail.com>
2018-10-29 15:58:35 +0530

    Updations

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://github.com/PravinWork/ru-active-campaign)
+
 # ru-active-campaign
 Repository for plugin of ActiveCampaign. It adds the registered user in wordpress to ActiveCampaign's list.
 
+Also, a automatic control on sync users. Just select you list id, and little settings your plugin is ready to sync the users.
+
+### Requirements 
+- API URL
+- API key
+- List Id if you need to sync the data with different list.
+
 ### Usage
 
+if trigger the sync action manually
 Place this code where your custom rgistration form is submitted. just after creating the user and updated its meta information. 
 
 ```sh
 do_action( 'rusac_add_new_address' , $user_id );
 ```
 Its very good procedure for custom registration forms.
+
+If you want it should sync the data automatically. Just make sync settings on and do some settings.
 
 ### Hooks
 Plugin is having multiple hooks to gain more control on plugin's data
--- rus-activecampaign.php
+++ rus-activecampaign.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Registered User Sync ActiveCampaign
-Plugin URI: http://www.activecampaign.com/apps/wordpress
+Plugin URI: https://wordpress.org/plugins/rus-activecampaign/
 Description: Allows you to sync registered users to the Active Campaigns Email Marketing app. It will auto sync the user who is added by register_user() method.
 Author: Pravin Durugkar
 Version: 1.0
@@ -17,14 +17,20 @@ define( 'rusac_URI', plugin_dir_url( __FILE__ ), true );
 class RUSActiveCampaign {
     
     function __construct(){      
- 
+        $sync_switch = get_option('rusac_sync_switch',0);
+
         add_action( 'admin_init', array($this, 'rusac_register_settings' ) );
         add_action('admin_menu', array($this, 'rusac_register_options_page') );
 
-        //add_action('user_register', array($this, 'rusac_send_data_to_ac'));
-        //profile_update
-        //add_action('profile_update', array($this, 'rusac_send_data_to_ac'));
         add_action('rusac_add_new_address', array($this, 'rusac_send_data_to_ac'));
+
+        if ($sync_switch) { 
+            $this->rusac_set_sync_with_activecampaign();
+            // the registered cron hook, that'll fire your function
+            add_action('rusac_sync_users_with_active_campaign', array($this, 'sync_active_campaign_list'));
+        }else{
+            $this->rusac_clear_sync_with_activecampaign();
+        }
     }
 
     function rusac_load_api_details() {
@@ -71,17 +77,21 @@ class RUSActiveCampaign {
     function rusac_prepare_registered_user_data($user_id) {
         // here we define the data we are posting in order to perform an update
         $user_obj = get_userdata($user_id);
+        
         $lang_tags = ($user_info->i_speak == '') ? 'English' : $user_info->i_speak;
         //$lang_tags == 'en'
         switch ($lang_tags) {
             case 'en':
                 $lang_tags = 'English';
                 break;
+            case 'english':
+                $lang_tags = 'English';
+                break;
             case 'fr':
                 $lang_tags = 'French';
                 break;
-            case 'fr':
-                $lang_tags = 'English';
+            case 'french':
+                $lang_tags = 'French';
                 break;
             default:
                 $lang_tags = 'English';
@@ -166,13 +176,10 @@ class RUSActiveCampaign {
         curl_setopt($request, CURLOPT_HEADER, 0); // set to 0 to eliminate header info from response
         curl_setopt($request, CURLOPT_RETURNTRANSFER, 1); // Returns response data instead of TRUE(1)
         curl_setopt($request, CURLOPT_POSTFIELDS, $data); // use HTTP POST to send form data
-        //curl_setopt($request, CURLOPT_SSL_VERIFYPEER, FALSE); // uncomment if you get no gateway response and are using HTTPS
         curl_setopt($request, CURLOPT_FOLLOWLOCATION, true);
 
         $response = (string)curl_exec($request); // execute curl post and store results in $response
 
-        // additional options may be required depending upon your server configuration
-        // you can find documentation on curl options at http://www.php.net/curl_setopt
         curl_close($request); // close curl object
 
         if ( !$response ) {
@@ -183,6 +190,72 @@ class RUSActiveCampaign {
         // unserializer
         $result = unserialize($response);
         do_action('rusac_after_sent_data_to_ac', $result);
+        return $result;
+    }
+
+    function rusac_set_sync_with_activecampaign() {
+        // add a 60 second cron schedule
+        add_filter('cron_schedules', function ( $schedules ) {
+            $schedules['everyminute'] = array(
+                'interval' => 60,
+                'display' => __('Every Minute')
+            );
+            return $schedules;
+        });
+
+        // register your cronjob
+        add_action('wp', function () {
+            if ( !wp_next_scheduled( 'rusac_sync_users_with_active_campaign' ) )
+                wp_schedule_event(time(), 'everyminute', 'rusac_sync_users_with_active_campaign');
+        });
+
+        /*// the registered cron hook, that'll fire your function
+        add_action('my_task_sync_cronjob', 'my_task_sync_method');*/
+    }
+
+    function rusac_clear_sync_with_activecampaign() {
+        // add a 60 second cron schedule
+        wp_clear_scheduled_hook( 'rusac_sync_users_with_active_campaign' );
+    }
+
+    function sync_active_campaign_list() {
+        //sync logic   //_rusac_synced_user
+
+        //fetch user setting
+        $number_of_users = get_option('rusac_no_users_per_run', 5);
+        $allowed_roles = get_option('rusac_allowed_roles');
+        $args = array(
+                    //'role' => 'subscriber',
+                    'meta_query' => array(
+                                        array(
+                                             'key' => '_rusac_synced_user',
+                                             'compare'  => 'NOT EXISTS'
+                                        )
+                                    ),
+                    'role__in' => $allowed_roles
+                );
+        $users = get_users( $args );
+
+        $count = 0;
+        if(!empty($users)){
+            foreach ( $users as $user ) {
+                $result = $this->rusac_send_data_to_ac($user->ID);
+                
+                echo '<pre>'; print_r($result['result_message']); echo '</pre>';
+                
+                if (!empty($result[result_code] == 1)) {
+                    update_user_meta( $user->ID, '_rusac_synced_user', $result);
+                }elseif(stripos($result['result_message'] , 'list that does not allow duplicates') !== false ){
+                    var_dump( $user->ID );
+                    update_user_meta( $user->ID, '_rusac_synced_user', $result);
+                }
+                if(++$count >= $number_of_users) { break; }
+            }
+        }
+    }
+    
+    function convert_user_id($user) {
+        return $user->ID;
     }
 
     function rusac_register_settings() {
@@ -197,21 +270,39 @@ class RUSActiveCampaign {
         $list_id = get_option('rusac_list_id');
         if(empty($list_id)) {
             add_option( 'rusac_list_id', '1');
-        } 
+        }
+        $sync_switch = get_option('rusac_sync_switch');
+        if(empty($sync_switch)) {
+            add_option( 'rusac_sync_switch', 0);
+        }
+        $rusac_no_users_per_run = get_option('rusac_no_users_per_run');
+        if(empty($rusac_no_users_per_run)) {
+            add_option( 'rusac_no_users_per_run', 5);
+        }
+        $rusac_allowed_roles = get_option('rusac_allowed_roles');
+        if (empty($rusac_allowed_roles)) {
+            add_option( 'rusac_allowed_roles' , array('subscriber'));
+        }
         register_setting( 'rusac_options_group', 'rusac_api_url', 'rusac_callback' );
         register_setting( 'rusac_options_group', 'rusac_api_key', 'rusac_callback' );
         register_setting( 'rusac_options_group', 'rusac_list_id', 'rusac_callback' );
+        register_setting( 'rusac_options_group', 'rusac_sync_switch', 'rusac_callback' );
+        register_setting( 'rusac_options_group', 'rusac_no_users_per_run', 'rusac_callback' );
+        register_setting( 'rusac_options_group', 'rusac_allowed_roles', 'rusac_callback' );
     }
     function rusac_register_options_page() {
         add_options_page('RU Active Campaigns Settings', 'RU Active Campaigns Settings', 'manage_options', 'rusac-settings', array($this,'rusac_options_page'));
     }
 
-    function rusac_options_page() { 
+    function rusac_options_page() {
+        global $wp_roles;
     ?>
     <div class="rusac-settings-section">
         <style type="text/css">
         form.rusac-settings input[type="text"]{padding:5px 10px;font-size:14px;width:300px;max-width:100%;}
         form.rusac-settings label{font-size:14px;line-height:30px;}
+        form.rusac-settings .rusac-sync-settings label{padding-right:15px;}
+        form.rusac-settings .allowed-roles label{padding-right:15px;}
         </style>
         <?php screen_icon(); ?>
         <h2><?php echo _e('RU Active Campaigns Settings','rusac');?></h2>
@@ -231,6 +322,26 @@ class RUSActiveCampaign {
                     <th scope="row"><label for="rusac_list_id"><?php echo _e('Listing ID','rus-activecampaign');?></label></th>
                     <td><input type="text" id="rusac_list_id" name="rusac_list_id" value="<?php echo (get_option('rusac_list_id')); ?>" /> </td>
                 </tr>
+                <tr valign="top">
+                    <th scope="row"><label for="rusac_sync_switch"><?php echo _e('Auto Sync','rus-activecampaign');?></label></th>
+                    <td class="rusac-sync-settings"><?php $sync_option = get_option('rusac_sync_switch');?>
+                        <label for="rusac_sync_switch_on"><input type="radio" id="rusac_sync_switch_on" name="rusac_sync_switch" value="1" <?php if ($sync_option == 1): ?> checked <?php endif ?>> On</label>
+                       <label for="rusac_sync_switch_off"> <input type="radio" id="rusac_sync_switch_off" name="rusac_sync_switch" value="0" <?php if ($sync_option == 0): ?>checked <?php endif ?>> Off</label>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><label for="rusac_no_users_per_run"><?php echo _e('Number Of Users Sync per run','rus-activecampaign');?></label></th>
+                    <td class="rusac-sync-settings"><input type="number" min=0 max=100 name="rusac_no_users_per_run" id="rusac_no_users_per_run" value="<?php echo get_option('rusac_no_users_per_run'); ?>"></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><label for="allowed_user_roles">Allow user roles to be sync</label></th>
+                    <td class="allowed-roles">
+                    <?php $all_roles = $wp_roles->roles;
+                    $allowed_roles = get_option('rusac_allowed_roles');
+                    foreach ($all_roles as $key => $value) { ?>
+                        <label for="role_<?php echo $key;?>"><input type="checkbox" id="role_<?php echo $key;?>" name="rusac_allowed_roles[]" value="<?php echo $key; ?>" <?php if (in_array($key,$allowed_roles)): ?> checked <?php endif ?>> <?php echo $value['name']; ?></label>
+                    <?php }
+                ?></td></tr>
             </table>
             <?php  submit_button(); ?>
         </form> 
